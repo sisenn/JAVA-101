33aab38907acab7f33de8a17c46773489009bc4d
Sinem Şen <sinemsen@Sinem-Air.home>
2024-01-29 02:49:41 +0300

    prime

---
+++ PrimeNumbers.java
@@ -0,0 +1,19 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        for (int number = 2; number <= 100; number++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+}
